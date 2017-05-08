82ac0124d11f92b734604e5ab11607c9a0fa22c3
Maciek Grzybek <maciek@dsemotion.com>
2017-05-08 13:54:20 +0100

    Added default opitons

--- awesome-num-animation.js
+++ awesome-num-animation.js
@@ -19,15 +19,20 @@
     
     function awesomeScroll(element,options){
 
-        //default options 
-        if ( !options ) {
-            options = {
-                startNumber : 0,
-                maxNumber : 100,
-                duration : 1500,
-                reveal : 150
-            }
+        // Default values if user won't add any options
+        var DEFAULTS = {
+            startNumber : 0,
+            maxNumber : 100,
+            duration : 1500,
+            reveal : 150
         };
+
+        // Values added by user in function invokation
+        var config = options;
+
+        // Final values used in function
+        // Assign function takes user's options (if they exist) and change them in default options
+        var settings = Object.assign({}, DEFAULTS, config);
 
         window.addEventListener('scroll', function(e){
 
@@ -45,7 +50,7 @@
             
             // Function that calculate position ov viewport
             (function calculatePosition(){
-                if (distanceToTop-windowHeight-scrollY < -(options.reveal) && animated==false) {
+                if (distanceToTop-windowHeight-scrollY < -(settings.reveal) && animated==false) {
                     animateNumber();
                     animated = true; // Set to true, so element is animated only once
                 }
@@ -53,19 +58,19 @@
 
             function animateNumber(){
 
-                var range = options.maxNumber - options.startNumber; // differnce between animated numbers
+                var range = settings.maxNumber - settings.startNumber; // differnce between animated numbers
 
-                var currentNumber = options.startNumber; // setting a starting number in animation
+                var currentNumber = settings.startNumber; // setting a starting number in animation
 
-                var increment = options.maxNumber > options.startNumber ? 1 : -1; // setting animation 'direction'
+                var increment = settings.maxNumber > settings.startNumber ? 1 : -1; // setting animation 'direction'
 
-                var step = Math.abs(Math.floor(options.duration/range)); // setting time step for animation
+                var step = Math.abs(Math.floor(settings.duration/range)); // setting time step for animation
 
                 // actual animation part
                 var intervalAnimation = setInterval(function(){
                     currentNumber += increment;
                     document.querySelector(element).innerHTML = currentNumber;
-                    if (currentNumber == options.maxNumber) {
+                    if (currentNumber == settings.maxNumber) {
                         clearInterval(intervalAnimation);
                     }
                 },step);
