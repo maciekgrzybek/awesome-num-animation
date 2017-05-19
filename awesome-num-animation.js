cc0439468fa4d013c698b1ec4438661b2bdb04c8
Maciek Grzybek <maciek@dsemotion.com>
2017-05-19 13:07:53 +0100

    .

--- awesome-num-animation.js
+++ awesome-num-animation.js
@@ -4,96 +4,96 @@
   'use strict';
 
   if (typeof define === 'function' && define.amd) {
-    define(factory);
+	define(factory);
   } else if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = factory();
+	module.exports = factory();
   } else {
-    window.awesomeNumAnimation = factory();
+	window.awesomeNumAnimation = factory();
   }
 
 })(function(){
 
-    'use strict';
+	'use strict';
 
-    var animated = false;
-    
-    function awesomeNumAnimation(element,options){
+	var animated = false;
+	
+	function awesomeNumAnimation(element,options){
 
-        var DEFAULTS = {
-            startNumber : 0,
-            maxNumber : 100,
-            duration : 2000,
-            reveal : 150
-        };
+		var DEFAULTS = {
+			startNumber : 0,
+			maxNumber : 100,
+			duration : 2000,
+			reveal : 150
+		};
 
-        var config = options;
-       
-        // Final values used in function
-        // Assign function takes user's options (if they exist) and change them in default options
-        var settings = Object.assign({}, DEFAULTS, config);
+		var config = options;
+	   
+		// Final values used in function
+		// Assign function takes user's options (if they exist) and change them in default options
+		var settings = Object.assign({}, DEFAULTS, config);
 
-        var starttime;
-        
+		var starttime;
+		
 
-        // Main animating function
-        function animateNumber(timestamp, element, duration, startNumber, maxNumber){
+		// Main animating function
+		function animateNumber(timestamp, element, duration, startNumber, maxNumber){
 
-            var range = maxNumber - startNumber; 
+			var range = maxNumber - startNumber; 
 
-            var timestamp = timestamp || new Date().getTime(); //if browser doesn't support requestAnimationFrame, generate own timestamp
+			var timestamp = timestamp || new Date().getTime(); //if browser doesn't support requestAnimationFrame, generate own timestamp
 
-            var runtime = timestamp - starttime;
+			var runtime = timestamp - starttime;
 
-            var progress = runtime / duration;
+			var progress = runtime / duration;
 
-            progress = Math.min(progress, 1); // Ensuring animation won't go over Max Number
-            
-            var currentNumber = startNumber + parseInt((range * progress).toFixed(0));
-            
-            element.innerHTML = currentNumber; 
+			progress = Math.min(progress, 1); // Ensuring animation won't go over Max Number
+			
+			var currentNumber = startNumber + parseInt((range * progress).toFixed(0));
 
-            if (runtime < duration){ 
+			element.innerHTML = currentNumber; 
 
-                requestAnimationFrame(function(timestamp){ 
+			if (runtime < duration){ 
 
-                    animateNumber(timestamp, element, duration, startNumber, maxNumber)
+				requestAnimationFrame(function(timestamp){ 
 
-                })
-            }
-        };
-        
-        // What to do on scroll
-        function onScroll(e) {
+					animateNumber(timestamp, element, duration, startNumber, maxNumber)
 
-            var animateDiv = document.querySelector(element);
-            var scrollY = window.pageYOffset;
-            var distanceToTop = animateDiv.getBoundingClientRect().top + scrollY;
-            var windowHeight = window.innerHeight;
-            
-            (function(){
-                if (distanceToTop - windowHeight - scrollY < - (settings.reveal)) {
+				})
+			}
+		};
+		
+		// What to do on scroll
+		function onScroll(e) {
 
-                    requestAnimationFrame(function(timestamp){
+			var animateDiv = document.querySelector(element);
+			var scrollY = window.pageYOffset;
+			var distanceToTop = animateDiv.getBoundingClientRect().top + scrollY;
+			var windowHeight = window.innerHeight;
+			
+			(function(){
+				if (distanceToTop - windowHeight - scrollY < - (settings.reveal)) {
 
-                        starttime = timestamp || new Date().getTime(); //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date
+					requestAnimationFrame(function(timestamp){
 
-                        animateNumber(timestamp, animateDiv, settings.duration, settings.startNumber, settings.maxNumber); // Calling animation with proper options
-                        
-                    });
-                    animated = true; // Set to true, so element is animated only once
-                    window.removeEventListener('scroll', onScroll);
-                }
-            })();
-            
-        };
+						starttime = timestamp || new Date().getTime(); //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date
 
-        if (animated == false) {
-            window.addEventListener('scroll', onScroll);
-        }
-         
-         
-    };
-    return awesomeNumAnimation;
+						animateNumber(timestamp, animateDiv, settings.duration, settings.startNumber, settings.maxNumber); // Calling animation with proper options
+						
+					});
+					animated = true; // Set to true, so element is animated only once
+					window.removeEventListener('scroll', onScroll);
+				}
+			})();
+			
+		};
+
+		if (animated == false) {
+			window.addEventListener('scroll', onScroll);
+		}
+		 
+		 
+	};
+	return awesomeNumAnimation;
 });
 
 
